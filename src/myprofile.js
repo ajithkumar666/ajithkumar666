bd29a06d42d2a6cdfacfe36a3011ff5a65c3a709
Ajithkumar M <ajith721@gmail.com>
2024-03-09 18:41:52 +0530

    content updated

--- src/myprofile.js
+++ src/myprofile.js
@@ -1,22 +1,31 @@
 // Menu links
 let getAbout = document.getElementById("getAbout");
 let getResume = document.getElementById("getResume");
+let getSkills = document.getElementById("getSkills");
+let getEducation = document.getElementById("getEducation");
 let getContact = document.getElementById("getContact");
 
 // Sections
 let about = document.getElementById("about");
 let resume = document.getElementById("resume");
+let skills = document.getElementById("skills");
+let education = document.getElementById("education");
 let contact = document.getElementById("contact");
+
 
 function removeClass() {
     // Links
     getAbout.classList.remove('selected');
     getResume.classList.remove('selected');
     getContact.classList.remove('selected');
+    getEducation.classList.remove('selected');
+    getSkills.classList.remove('selected');
     // Sections
     about.classList.remove('view');
     resume.classList.remove('view');
     contact.classList.remove('view');
+    education.classList.remove('view');
+    skills.classList.remove('view');
 }
 
 getAbout.addEventListener('click', function (e) {
@@ -44,3 +53,22 @@ getContact.addEventListener('click', function (e) {
         getContact.classList.add('selected');
     }
 })
+
+getEducation.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        education.classList.add('view');
+        getEducation.classList.add('selected');
+    }
+})
+
+getSkills.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        skills.classList.add('view');
+        getSkills.classList.add('selected');
+    }
+})
+
